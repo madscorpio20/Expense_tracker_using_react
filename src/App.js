d6f79c68e844de2cb067fc9c7ae8db8d262bac1e
madscorpio20 <madhavkumarrai@gmail.com>
2022-06-30 07:33:08 +0530

    expense tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+ import "./App.css";
+function App(){
+    let expense = [
+        {
+            id: 'e1',
+            title: 'College fess',
+            amount: 250,
+            date: new Date(2022 , 6, 29)
+        },
+        {
+            id: 'e2',
+            title: 'Books',
+            amount: 50,
+            date: new Date(2022 , 6, 29)
+        },
+        {
+            id: 'e3',
+            title: 'Food',
+            amount: 120,
+            date: new Date(2022 , 6, 29)
+        },
+        {
+            id: 'e4',
+            title: 'Travel',
+            amount: 60,
+            date: new Date(2022 , 6, 29)
+        },
+        {
+            id: 'e5',
+            title: 'House rent',
+            amount: 800,
+            date: new Date(2022 , 6, 29)
+        },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    ]
+    return (
+        <div>
+            <h1 className="heading">Expense Tracker</h1>
+            <Expenses item = {expense}/>
+        </div>
+        
+    );
 }
-
 export default App;
